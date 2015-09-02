b8dbd68c80979e51faec5c57737960e2ea3d5910
alui <al.chevere@icloud.com>
2015-09-01 20:11:05 -0500

    Proyect Structure ready.

---
+++ src/mx/unam/ciencias/edd/Proyecto1.java
@@ -0,0 +1,25 @@
+package mx.unam.ciencias.edd;
+
+import java.text.NumberFormat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Proyecto 1: Graficador.
+ */
+public class Proyecto1 {
+    
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
