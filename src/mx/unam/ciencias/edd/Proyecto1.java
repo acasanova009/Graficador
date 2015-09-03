ac5ae8879665b91b311dcf2825c9b1d1f3bc1e10
alui <al.chevere@icloud.com>
2015-09-02 22:05:32 -0500

    Estructurando el futuro del proyecto.

--- src/mx/unam/ciencias/edd/Proyecto1.java
+++ src/mx/unam/ciencias/edd/Proyecto1.java
@@ -20,6 +20,21 @@ public class Proyecto1 {
     public static void main(String[] args)
     {
         
+        //Dada una linea de texto con formato UTF-8.
+        
+        //Analizar la linea con el analizador léxico, y regresar un lista de fichas.
+        //Cada ficha contiene el valor real, y la gramatica correspondiente.
+    
+        //Con la lista de fichas, analizaremos la sintactica con el analizador sintactico utilizando la gramatica posfija, y crearemos el arbol sintactico.
+        //Este arbol tendra la capacidad de auto evaluarse en x. Generando un f(x)=arbolSintactico.evaluarEn(x).
+        
+        //Finalmente cargaremos la vista a partir de conectar el arbol sintactico con las escuchas de la interfaz grafica de java. Para que podamos pintar el grafica las veces que sea necesario.
+        
+        
+        //Clases por implementar. Fichas. Analizador Lexico. Analizador Sintactico. Arbol Sintactico, que por tener expresiones algebraicas, es binario.
+        
+        
+        
     }
     
 }
